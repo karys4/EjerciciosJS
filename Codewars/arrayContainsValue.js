7e1146b953f199bf694ae8dd9862c94c83355bcf
karys4 <kary.github@gmail.com>
2022-09-07 23:52:50 -0500

    Uso de metodo includes

---
+++ Codewars/arrayContainsValue.js
@@ -0,0 +1,20 @@
+/*
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+Array can contain numbers or strings. X can be either.
+Return true if the array contains the value, false if not.
+*/
+
+function check(a, x) {
+  a.includes(x) ? console.log(true) : console.log(false);
+}
+
+check([66, 101], 66); //true
+check([101, 45, 75, 105, 99, 107], 107); //true
+check(["t", "e", "s", "t"], "e"); //true
+check(["what", "a", "great", "kata"], "kat"); //false;
+
+//Respuesta optimizada
+
+function check(a, x) {
+  return a.includes(x);
+}
